383c42cf003b7e539bc645e59b4c67a8bdebc9e8
Fuflik1752 <sas.chikunova@mail.ru>
2024-06-09 21:43:40 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_hw = 12
+hours_spent = 1.5
+course_name = 'Python'
+execution_time = hours_spent / completed_hw
+print('Курс:',course_name,', всего задач:',completed_hw,', затрачено часов:',hours_spent,', среднее время выполнения',execution_time,'часа')
