efec7af1f12613bc4017fd5b2c32579493a44887
Fuflik1752 <sas.chikunova@mail.ru>
2024-06-12 16:38:42 +0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_dict = {'Masha': 2008, 'Misha': 2011, 'Marina': 2002}
+print(my_dict)
+print(my_dict['Masha'])
+my_dict['Sasha'] = 2009
+print(my_dict)
+my_dict['Koly'] = 2015
+my_dict['Oly'] = 2004
+print(my_dict)
+del my_dict['Marina']
+print(my_dict)
+my_dict['Marina'] = 2002
+print(my_dict)
